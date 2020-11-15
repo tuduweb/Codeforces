35bcfff2f85b32f73336c8746c42c7044fdeb046
tududweb <tuduweb@qq.com>
2020-11-16 00:55:21 +0800

    错误：正常通过二分查找到“合适”的位置，再循环更新贪心列。

--- 1437/E/E.cpp
+++ 1437/E/E.cpp
@@ -14,7 +14,7 @@ int FindLIS(int* array, int length)
     vector<int > d(length);
     int bound = array[length - 1];
     int len = 0;
-    d[0] = array[0];
+    d[0] = 0;
     for(int i = 1; i < length - 1; ++i)
     {
         if(array[i] < array[0] + i)
@@ -24,24 +24,32 @@ int FindLIS(int* array, int length)
     for(int i = 1; i < length - 1; ++i)
     {
         //大于上边界,这个值可以直接忽略了
+        cout << i << "| d[len]=" << d[len] << " array[i]=" << array[i] <<" array[d[len]]=" << array[d[len]] << endl;
+
+        for(int j = 1; j <= len; ++j)
+            cout << array[d[j]] << " ";
+        cout << endl;
+
         if(array[i] >= bound)
             continue;
+
+        //cout << "go on" << endl;
         
-        if(array[i] > d[len])
+        if(array[i] > array[d[len]] && (array[i] - array[d[len]] >= i - d[len]))
         {
             // if(array[i] < array[i - 1] + i)
             //     continue;
             //贪心,比上一个长度大一些,进位
-            d[++len] = array[i];
-        }else if(array[i] == d[len]){
+            d[++len] = i;
+        }else if(array[i] == array[d[len]]){
             continue;
         }else{
-            //小,那么满足尽可能小的情况,二分查找“刚刚好”的位置
-            int l = 1, r = len, pos = 0;
+            //小,那么满足尽可能小的情况,二分查找已经存储的“刚刚好”的位置
+            int l = 0, r = len, pos = 0;
             while(l <= r)
             {
                 int mid = (l + r) / 2;
-                if(d[mid] < array[i])
+                if(array[d[mid]] - array[i] < 0)
                 {
                     //还太大了,那么作为左边界
                     pos = mid;
@@ -51,8 +59,16 @@ int FindLIS(int* array, int length)
                     r = mid - 1;
                 }
             }
-            //二分查找完毕,pos + 1为所找
-            d[pos + 1] = array[i];
+
+            //d[pos + 1] -> 0 全更新
+            
+            for(int j = pos + 1; j > 0; --j)
+            {
+                cout << "j = " << j << " array[d[j - 1]]=" <<array[d[j - 1]]  << endl;
+                if(array[i] - array[d[j - 1]] >= i - d[j - 1] && ( (j == pos + 1) || (array[i] - array[d[j]] < i - d[j])))
+                    d[j] = i;
+            }
+            
         }
     }
 
@@ -62,6 +78,12 @@ int FindLIS(int* array, int length)
     }
 
     cout << "| lis:" << len << endl;
+
+    for(int i = 0; i < len + 1; ++i)
+    {
+        cout << array[d[i]] << " ";
+    }
+    cout << endl;
 
     
 
@@ -151,3 +173,9 @@ int main()
 
     return 0;
 }
+
+/** err
+ *
+100 0
+4 20 52 2 7 36 8 73 9 99 13 19 14 89 32 2 6 2 16 21 47 22 66 23 25 95 28 4 29 64 30 31 7 48 32 51 35 37 77 57 40 42 56 40 43 44 46 13 47 12 48 49 24 51 69 53 54 98 21 57 59 61 62 52 63 30 68 32 69 39 8 67 71 59 72 81 76 77 11 82 85 14 83 33 86 89 89 90 43 96 91 31 93 69 58 94 45 96 2 99
+*/
