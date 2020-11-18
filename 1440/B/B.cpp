b906f03bb23f0a17f2c54b473ab4622f9fba75a2
tududweb <tuduweb@qq.com>
2020-11-19 00:57:33 +0800

    数学归纳方法完成1440(B)题目。

--- 1440/B/B.cpp
+++ 1440/B/B.cpp
@@ -8,6 +8,9 @@ using namespace std;
  * 是需要找到maximum的值
  * 
  * 需要找到最大值,那么medians以下的值应该是
+ * 
+ * 贪心策略 那么从最大值开始填坑,可以找到通项公式
+ * n*k - (n - medians) * p = i (p = 1,2,……,k)
  */
 int main()
 {
@@ -19,25 +22,24 @@ int main()
         int n,k;
         cin >> n >> k;
         
-        vector<int > v(1000);//non-decreasing
-        int temp;
-        int sum = 0, medians = (n + 1) / 2 - 1;
+        //vector<int > v(1000);//non-decreasing
+        __int64 temp = 0,sum = 0;
+        int medians = (n + 1) / 2 - 1;
 
-        for(int i = 0; i < k; ++i)
+        for(int i = 0; i < k*n; ++i)
         {
 
-            for(int j = 0; j < n; ++j)
+            cin >> temp;
+            if(((n*k - i) % (n - medians)== 0) && i >= (n*k - k*(n - medians)))
             {
-                cin >> temp;
-                if(j == medians)
-                {
-                    sum += temp;
-                }
+                sum += temp;
+                //cout << temp << " ";
             }
         }
+        //cout << endl;
 
-        cout << "medians: " << medians << "  sum: " << sum << endl;
-
+        //cout << "medians: " << medians << "  sum: " << sum << endl;
+        cout << sum << endl;
     }
 
     return 0;
