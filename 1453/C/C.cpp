afdaee2a7de585f55651cd268e495bc5e1dea52f
bin <tuduweb@qq.com>
2020-12-06 14:02:50 +0800

    1453(C)AC，思路模糊，看了油管主的思路。

--- 1453/C/C.cpp
+++ 1453/C/C.cpp
@@ -1,16 +1,18 @@
 /**
  * https://codeforces.com/contest/1453/problem/C
+ * 想破头也想不出来,只能想到最拉跨的方法..但写起来一定特别麻烦
+ * 视频: https://www.youtube.com/watch?v=TZhy-BIIO-Q (印度人…)
  **/
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
 
 
 using namespace std;
 
 struct point{
-    int x;
-    int y;
+    int c;
+    int r;
 };
 
 void solve(vector<vector<int > > v)
@@ -20,7 +22,7 @@ void solve(vector<vector<int > > v)
 
 void solve_line(int n, vector<struct point >* v)
 {
-    int result[10];
+    vector<int > ans(10, 0);
     for(int digit = 0; digit < 10; ++digit)
     {
         if(v[digit].size() < 2)
@@ -29,11 +31,37 @@ void solve_line(int n, vector<struct point >* v)
             continue;
         }
 
-
-
+        //关键点,最最最靠近四个边的点
+        struct point    minC = {n, n},
+                        maxC = {-1, -1},
+                        minR = {n, n},
+                        maxR = {-1, -1};
+        #define P v[digit][i]
+        for(int i = 0; i < v[digit].size(); ++i)
+        {
+            if(P.c < minC.c) minC = P;
+            if(P.c > maxC.c) maxC = P;
+            if(P.r < minR.r) minR = P;
+            if(P.r > maxR.r) maxR = P;
+        }
+        // cout << "minC(" << minC.c <<  "," << minC.r << ")" << endl;
+        // cout << "maxC(" << maxC.c <<  "," << maxC.r << ")" << endl;
+        // cout << "minR(" << minC.c <<  "," << minR.r << ")" << endl;
+        // cout << "maxR(" << maxR.c <<  "," << maxR.r << ")" << endl;
+        //所有位置的遍历,取出最大面积ans
+        for(int i = 0; i < v[digit].size(); ++i)
+        {
+            ans[digit] = max(ans[digit], max(P.c - minC.c, maxC.c - P.c) * max(P.r, n - 1 - P.r));
+            ans[digit] = max(ans[digit], max(P.r - minR.r, maxR.r - P.r) * max(P.c, n - 1 - P.c));
+        }
         //
-
     }
+
+    for(int i = 0; i < 10; ++i)
+    {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
 }
 
 int main()
@@ -77,19 +105,19 @@ int main()
                 if(inputChar >= '0' && inputChar <='9')
                 {
                     inputChar -= '0';
-                    v[inputChar].push_back({c,r});
+                    v[inputChar].push_back({r,c});
                     c++;
                 }
             }
         }
 
-        for(int i = 0; i < 10; ++i)
-        {
-            cout << "[" << i << "] ";
-            for(int j = 0; j < v[i].size(); ++j)
-                cout << "(" << v[i][j].x << "," << v[i][j].y << ") ";
-            cout << endl;
-        }
+        // for(int i = 0; i < 10; ++i)
+        // {
+        //     cout << "[" << i << "] ";
+        //     for(int j = 0; j < v[i].size(); ++j)
+        //         cout << "(" << v[i][j].c << "," << v[i][j].r << ") ";
+        //     cout << endl;
+        // }
 
         solve_line(n, v);
 
