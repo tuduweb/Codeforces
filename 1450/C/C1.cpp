10bb81cc293f12f37953badc6e8337994d84cae1
tududweb <tuduweb@qq.com>
2020-12-18 00:06:18 +0800

    第一版思路：横，竖分别扫描，构建扫描程序。

--- 1450/C/C1.cpp
+++ 1450/C/C1.cpp
@@ -13,6 +13,67 @@ void solve(int n, vector<vector<char > >& v)
             cout << v[i][j] << " ";
         cout << endl;
     }
+
+    vector<vector<pair<int ,int > > > vInCol(n);
+    vector<vector<pair<int ,int > > > vInRow(n);
+
+    for(int col = 0; col < n; ++col)
+    {
+        int k = 0;
+        for(int row = 0; row < n; ++row)
+        {
+            if(v[row][col] == 'X')
+                k++;
+            else{
+                if(k > 2)
+                {
+                    //当前为空 从上一个 到 上一个 - k
+                    vInCol[col].push_back({row - k, row});
+                }
+                k = 0;
+            }
+        }
+        //退出时
+        if(k > 2)
+        {
+            //当前为空 从上一个 到 上一个 - k
+            vInCol[col].push_back({n - k, n});
+        }
+    }
+
+    for(int row = 0; row < n; ++row)
+    {
+        int k = 0;
+        for(int col = 0; col < n; ++col)
+        {
+            if(v[row][col] == 'X')
+            {
+                k++;
+            }else{
+                if(k > 2)
+                {
+                    vInRow[row].push_back({col - k, col});
+                }
+                k = 0;
+            }
+        }
+        if(k > 2)
+        {
+            vInRow[row].push_back({n - k , n});
+        }
+    }
+
+
+    for(int i = 0; i < vInCol.size(); ++i)
+    {
+        for(int j = 0; j < vInCol[i].size(); ++j)
+        {
+            cout << "[" << vInCol[i][j].first << "," << vInCol[i][j].second << "]  ";
+        }
+        cout << endl;
+    }
+
+
 }
 
 int main()
