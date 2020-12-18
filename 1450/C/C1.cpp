be8c19c6792f043b4c47d739826d10632d233b4d
bin <tuduweb@qq.com>
2020-12-18 12:33:03 +0800

    根据官方题解完成，后面需要翻炒几次熟悉这类题。

--- 1450/C/C1.cpp
+++ 1450/C/C1.cpp
@@ -4,40 +4,68 @@
 using namespace std;
 
 
-
-void solve(int n, vector<vector<char > >& v)
+void offical_solve(int n, vector<vector<char> > & v)
 {
+    int cnt[3] = { 0, 0, 0};
+
+    //分类
     for(int i = 0; i < n; ++i)
     {
         for(int j = 0; j < n; ++j)
-            cout << v[i][j] << " ";
+        {
+            if(v[i][j] == 'X')
+                cnt[(i+j)%3]++;
+        }
+    }
+
+    int minCntPos = 0;
+    if(cnt[0] <= cnt[1] && cnt[0] <= cnt[2])
+        minCntPos = 0;
+    else if(cnt[1] <= cnt[0] && cnt[1] <= cnt[2])
+        minCntPos = 1;
+    else
+        minCntPos = 2;
+
+    for(int i = 0; i < n; ++i)
+    {
+        for(int j = 0; j < n; ++j)
+        {
+            if((i+j)%3 == minCntPos && v[i][j] == 'X')
+                v[i][j] = 'O';
+        }
+    }
+
+    for(int i = 0; i < n; ++i)
+    {
+        for(int j = 0; j < n; ++j)
+        {
+            cout << v[i][j];
+        }
         cout << endl;
     }
 
-    vector<vector<pair<int ,int > > > vInCol(n);
-    vector<vector<pair<int ,int > > > vInRow(n);
 
+}
+
+
+void solve(int n, vector<vector<char > >& v)
+{
     for(int col = 0; col < n; ++col)
     {
         int k = 0;
         for(int row = 0; row < n; ++row)
         {
             if(v[row][col] == 'X')
+            {
                 k++;
-            else{
-                if(k > 2)
+                if(k == 3)
                 {
-                    //当前为空 从上一个 到 上一个 - k
-                    vInCol[col].push_back({row - k, row});
+                    v[row][col] = 'O';
+                    k = 0;
                 }
+            }else{
                 k = 0;
             }
-        }
-        //退出时
-        if(k > 2)
-        {
-            //当前为空 从上一个 到 上一个 - k
-            vInCol[col].push_back({n - k, n});
         }
     }
 
@@ -49,30 +77,104 @@ void solve(int n, vector<vector<char > >& v)
             if(v[row][col] == 'X')
             {
                 k++;
-            }else{
-                if(k > 2)
+                if(k == 3)
                 {
-                    vInRow[row].push_back({col - k, col});
+                    v[row][col] = 'O';
+                    k = 0;
                 }
+            }else{
                 k = 0;
             }
         }
-        if(k > 2)
-        {
-            vInRow[row].push_back({n - k , n});
-        }
     }
 
+    //cout << endl << endl;
 
-    for(int i = 0; i < vInCol.size(); ++i)
+    for(int i = 0; i < n; ++i)
     {
-        for(int j = 0; j < vInCol[i].size(); ++j)
-        {
-            cout << "[" << vInCol[i][j].first << "," << vInCol[i][j].second << "]  ";
-        }
+        for(int j = 0; j < n; ++j)
+            cout << v[i][j];
         cout << endl;
     }
 
+    // vector<vector<pair<int ,int > > > vInCol(n);
+    // vector<vector<pair<int ,int > > > vInRow(n);
+
+    // for(int col = 0; col < n; ++col)
+    // {
+    //     int k = 0;
+    //     for(int row = 0; row < n; ++row)
+    //     {
+    //         if(v[row][col] == 'X')
+    //             k++;
+    //         else{
+    //             if(k > 2)
+    //             {
+    //                 //当前为空 从上一个 到 上一个 - k
+    //                 vInCol[col].push_back({row - k, k});
+    //             }
+    //             k = 0;
+    //         }
+    //     }
+    //     //退出时
+    //     if(k > 2)
+    //     {
+    //         //当前为空 从上一个 到 上一个 - k
+    //         vInCol[col].push_back({n - k, k});
+    //     }
+    // }
+
+    // for(int row = 0; row < n; ++row)
+    // {
+    //     int k = 0;
+    //     for(int col = 0; col < n; ++col)
+    //     {
+    //         if(v[row][col] == 'X')
+    //         {
+    //             k++;
+    //         }else{
+    //             if(k > 2)
+    //             {
+    //                 vInRow[row].push_back({col - k, k});
+    //             }
+    //             k = 0;
+    //         }
+    //     }
+    //     if(k > 2)
+    //     {
+    //         vInRow[row].push_back({n - k , k});
+    //     }
+    // }
+
+
+    // for(int i = 0; i < vInCol.size(); ++i)
+    // {
+    //     for(int j = 0; j < vInCol[i].size(); ++j)
+    //     {
+    //         cout << "[" << vInCol[i][j].first << "," << vInCol[i][j].second << "]  ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // for(int i = 0; i < vInRow.size(); ++i)
+    // {
+    //     for(int j = 0; j < vInRow[i].size(); ++j)
+    //     {
+    //         cout << "[" << vInRow[i][j].first << "," << vInRow[i][j].second << "]  ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // //找交点
+    // for(int i = 0; i < vInCol.size(); ++i)
+    // {
+    //     for(int j = 0; j < vInCol[i].size(); ++j)
+    //     {
+    //         //最少决策点数量 3,4,5 -> 1 6,7,8 -> 2 ..
+    //         //k/3
+    //     }
+    //     cout << endl;
+    // }
 
 }
 
@@ -98,7 +200,7 @@ int main()
 
 
 
-        solve(n, v);
+        offical_solve(n, v);
 
     }
 
